1d5266efb74cd19fb374e58a9eb72f2d30495c97
Manish Paul <manishpaul.dev@gmail.com>
2024-05-15 08:53:47 +0530

    added signin page

---
+++ lib/widgets/app_text_field.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class AppTextField extends StatefulWidget {
+  final bool isPassword;
+  final String hintText;
+
+  const AppTextField({
+    super.key,
+    required this.isPassword,
+    required this.hintText,
+  });
+
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  bool isPasswordVisible = false;
+
+  void togglePasswordVisibility() {
+    setState(() {
+      isPasswordVisible = !isPasswordVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.sizeOf(context).width;
+
+    return SizedBox(
+      width: deviceWidth * 0.30,
+      child: TextFormField(
+        obscureText: widget.isPassword && !isPasswordVisible,
+        style: const TextStyle(
+          color: Colors.black,
+        ),
+        cursorColor: Colors.purple[400],
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(
+            color: Colors.black54,
+          ),
+          contentPadding: const EdgeInsets.only(left: 10),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(
+              width: 2,
+              color: Colors.pink,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              width: 2,
+              color: Colors.purple[400]!,
+            ),
+          ),
+          suffixIcon: widget.isPassword
+              ? IconButton(
+                  onPressed: togglePasswordVisibility,
+                  icon: isPasswordVisible
+                      ? const Icon(Icons.visibility_outlined)
+                      : const Icon(Icons.visibility_off_outlined),
+                )
+              : const SizedBox.shrink(),
+        ),
+      ),
+    );
+  }
+}
