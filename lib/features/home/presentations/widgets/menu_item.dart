452980d3c249882ca2cd10421b30e78e6d8e15ea
Manish Paul <manishpaul.dev@gmail.com>
2024-05-17 21:24:52 +0530

    added highlights for menu items

--- lib/features/home/presentations/widgets/menu_item.dart
+++ lib/features/home/presentations/widgets/menu_item.dart
@@ -1,15 +1,18 @@
+import 'package:city_savvy_admin_panel/shared/styles/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class MenuItem extends StatelessWidget {
   final String menuTitle;
   final IconData menuIcon;
   final Function() changeView;
+  final bool isSelected;
 
   const MenuItem({
     super.key,
     required this.menuTitle,
     required this.menuIcon,
     required this.changeView,
+    required this.isSelected,
   });
 
   @override
@@ -20,7 +23,14 @@ class MenuItem extends StatelessWidget {
       onTap: changeView,
       child: Container(
         width: deviceWidth * 0.14,
-        margin: const EdgeInsets.symmetric(vertical: 10),
+        margin: const EdgeInsets.only(bottom: 5),
+        padding: const EdgeInsets.all(10),
+        decoration: isSelected
+            ? BoxDecoration(
+                color: AppColors.colorPrimary,
+                borderRadius: BorderRadius.circular(5),
+              )
+            : null,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -28,7 +38,7 @@ class MenuItem extends StatelessWidget {
               width: deviceWidth * 0.03,
               child: Icon(
                 menuIcon,
-                color: Colors.teal,
+                color: isSelected ? Colors.white : Colors.teal,
                 // size: 18,
               ),
             ),
@@ -37,9 +47,9 @@ class MenuItem extends StatelessWidget {
               width: deviceWidth * 0.08,
               child: Text(
                 menuTitle,
-                style: const TextStyle(
+                style: TextStyle(
                   fontSize: 20,
-                  color: Colors.teal,
+                  color: isSelected ? Colors.white : Colors.teal,
                 ),
               ),
             ),
