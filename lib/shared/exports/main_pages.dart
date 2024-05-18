05a03bb961cab363755194c7f5c65a4765110636
Manish Paul <manishpaul.dev@gmail.com>
2024-05-18 08:01:21 +0530

    added a file to export main pages to reduce imports

---
+++ lib/shared/exports/main_pages.dart
@@ -0,0 +1,10 @@
+export '../../features/ad/presentations/ad_page.dart';
+export '../../features/dashboard/presentations/dashboard_page.dart';
+export '../../features/home/presentations/home_page.dart';
+export '../../features/management/presentations/management.dart';
+export '../../features/management/presentations/users.dart';
+export '../../features/management/presentations/transport.dart';
+export '../../features/messages/presentations/messages_page.dart';
+export '../../features/notification/presentations/notifications.dart';
+export '../../features/settings/presentations/settings_page.dart';
+export '../../features/statistics/presentations/statistics.dart';
