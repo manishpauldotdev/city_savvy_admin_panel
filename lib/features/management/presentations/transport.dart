ae3f689eb00376a7bcb0ab1c5d033f763dfad286
Manish Paul <manishpaul.dev@gmail.com>
2024-05-18 08:00:37 +0530

    added a few required pages

---
+++ lib/features/management/presentations/transport.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TransportManagementPage extends StatefulWidget {
+  const TransportManagementPage({super.key});
+
+  @override
+  State<TransportManagementPage> createState() =>
+      _TransportManagementPageState();
+}
+
+class _TransportManagementPageState extends State<TransportManagementPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Transport Management');
+  }
+}
---
+++ lib/features/management/presentations/users.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UsersManagementPage extends StatefulWidget {
+  const UsersManagementPage({super.key});
+
+  @override
+  State<UsersManagementPage> createState() => _UsersManagementPageState();
+}
+
+class _UsersManagementPageState extends State<UsersManagementPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Users Management');
+  }
+}
